7441aefd8bd4c32a167b4e6b2b3f25d91dd75352
soerlemans <sebasoerlemans+git@gmail.com>
2026-03-20 00:39:45 +0100

    codegen: Working on popen, to clean up binding related stuff.

--- src/acris/codegen/cpp_backend/clang_frontend_invoker.cpp
+++ src/acris/codegen/cpp_backend/clang_frontend_invoker.cpp
@@ -1,6 +1,7 @@
 #include "clang_frontend_invoker.hpp"
 
 // STL Includes:
+#include <cstdio>
 #include <cstdlib>
 #include <format>
 #include <iomanip>
@@ -10,6 +11,7 @@
 
 // Local Includes:
 #include "acris/debug/log.hpp"
+#include "lib/stdexcept/stdexcept.hpp"
 #include "lib/stdtypes.hpp"
 #include "lib/string_util.hpp"
 
@@ -48,7 +50,7 @@ auto ClangFrontendInvoker::set_out(const std::string_view t_out) -> void
 }
 
 // Public Methods:
-auto ClangFrontendInvoker::compile(const fs::path &t_source) -> void
+auto ClangFrontendInvoker::compile(const fs::path& t_source) -> void
 {
   fs::path source{t_source};
   source.make_preferred();
@@ -108,5 +110,50 @@ auto ClangFrontendInvoker::compile(const fs::path &t_source) -> void
 
     // TODO: Throw an exception. Or maybe not?
   }
+}
+
+	// TODO: Test/fully implement.
+auto shell_exec(std::string t_cmd) -> ProcessResult
+{
+  using lib::stdexcept::RuntimeError;
+  using lib::stdexcept::throwf;
+
+  ProcessResult result{};
+  std::array<char, 512> buf{};
+
+  // Do an estimate for now.
+  result.m_stdout.reserve(10);
+
+#if defined(_WIN32)
+  auto* pipe = _popen(t_cmd.c_str(), "r");
+#else
+  auto* pipe = popen(t_cmd.c_str(), "r");
+#endif
+
+  if(!pipe) {
+    throwf<RuntimeError>("Failed to run command: {}", t_cmd);
+  }
+
+  std::string line{};
+  while(std::fgets(buf.data(), buf.size(), pipe) != nullptr) {
+    std::string line{buf.data()};
+
+    if(!line.empty() && line.back() == '\n') {
+      line.pop_back();
+
+      // We have a full line push back.
+      result.m_stdout.push_back(line);
+      line.clear();
+    }
+  }
+
+#if defined(_WIN32)
+  result.m_exit_code = _pclose(pipe);
+#else
+  int rc = pclose(pipe);
+  result.m_exit_code = WEXITSTATUS(rc);
+#endif
+
+  return result;
 }
 } // namespace codegen::cpp_backend
